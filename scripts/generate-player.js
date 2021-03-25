06a7dff7e9ae404c99d8029018357a7d5a042425
Erik Young <erik_young@mac.com>
2021-03-25 16:49:57 -0400

    Get generate-player script to work.

--- scripts/generate-player.js
+++ scripts/generate-player.js
@@ -1,11 +1,13 @@
-const PlayerToken = artifacts.require('PlayerToken')
+const PlayerTokenGenerator = artifacts.require('PlayerToken')
 
 module.exports = async callback => {
-  const svb = await PlayerToken.deployed()
-  console.log('Creating requests on contract:', dnd.address)
-  const tx = await svb.requestNewPlayerToken(77, "Kelvin Leerdam")
-  const tx2 = await svb.requestNewPlayerToken(7777777, "Another Player 1")
-  const tx3 = await svb.requestNewRandomPlayerToken(7, "Another Player 2")
-  const tx4 = await svb.requestNewRandomPlayerToken(777, "Another Player 3")
+  const generator = await PlayerTokenGenerator.deployed()
+  console.log('Creating requests on contract:', generator.address)
+
+  // The player token generator takes a number as a random seed and a player name.
+  const tx = await generator.requestNewRandomPlayerToken(875424, "Kelvin Leerdam")
+  const tx2 = await generator.requestNewRandomPlayerToken(7777777, "Another Player 1")
+  const tx3 = await generator.requestNewRandomPlayerToken(7, "Another Player 2")
+  const tx4 = await generator.requestNewRandomPlayerToken(777, "Another Player 3")
   callback(tx.tx)
 }
